78cd031c2bd05dab5baf04670c82001152a30c57
Jeffrey Lipton <jeffreyilipton@gmail.com>
2016-07-06 21:25:41 -0400

    structuerd message

--- CubeConnectivity/Cube.py
+++ CubeConnectivity/Cube.py
@@ -32,8 +32,8 @@ class Cube(object):
 
     def _faceLEDArray(self,id,cmd):
         '''sets the LEDs for face based on the cmd array of length 4'''
-        message = Int8MultiArray()
-        #### MAKE MESSAGE
+        vals = [int(x) for x in cmd]
+        message = "irled_%i_%i%i%i%i"%(id,vals[0],vals[1],vals[2],vals[3])
         self.pub(message)
         
 
